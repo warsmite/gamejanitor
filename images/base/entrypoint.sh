432d441ca8e1e32e72627c4af456c9b833c7799f
warsmite <warsmite@proton.me>
2026-03-14 04:02:06 +0000

    delete old spec file, delete unused stop-server scripts

--- images/base/entrypoint.sh
+++ images/base/entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-shutdown() {
-    /scripts/stop-server
-    wait "$SERVER_PID" 2>/dev/null
-    exit 0
-}
-trap shutdown SIGTERM SIGINT
-
 if [ ! -f /data/.installed ]; then
     echo "[entrypoint] running install-server"
     /scripts/install-server
