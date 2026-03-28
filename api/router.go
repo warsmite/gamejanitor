4092f8e03c59ad5e79ec38caf84859744171eed1
warsmite <warsmite@warsmite.com>
2026-03-28 02:09:26 +0000

    add Content-Security-Policy header

--- api/router.go
+++ api/router.go
@@ -264,6 +264,7 @@ func securityHeaders(next http.Handler) http.Handler {
 		w.Header().Set("X-Frame-Options", "DENY")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
+		w.Header().Set("Content-Security-Policy", "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self'")
 		next.ServeHTTP(w, r)
 	})
 }
